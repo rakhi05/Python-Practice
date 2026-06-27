698ad2036ce0ce4b8f5d34a27b3c16081bacf94e
rakhi05 <rakhi662006@gmail.com>
2026-06-27 12:55:22 +0530

    comments & seperator

---
+++ comments.py
@@ -0,0 +1,10 @@
+#single line comments
+## double line comments 
+### triple line comments
+
+#comments seperator and escape sequences learning
+
+print("hey i am 'a \"good boy\" \nand also a kind person")
+
+print("hey",6 ,7, sep="~",end="123\n") 
+
