7392154788c9f0b116d5b1007fbe10039588615c
rakhi05 <rakhi662006@gmail.com>
2026-06-27 13:10:06 +0530

    variables & datatypes

---
+++ variable.py
@@ -0,0 +1,29 @@
+# variable is a container store the data and changes are possible in it.
+
+
+a=100
+b="mannu"
+c=3.14
+
+print(a)
+print(b)
+print(c)
+print(a+c)
+
+# show the types od data store in a variable
+
+print("the type of a is",type(a))
+print("the type of b is",type(b))
+print("the type of c is",type(c))
+
+## the o/p show the types in class 'int' , class 'str' , class 'float' which represent the data type are represented as classes ##
+
+#in python programming language everything is an object and every object has a type and the type of an object is represented as a class.
+
+list=[ 1,2,3,[4,5,6],["apple,mango"]] #ordered and muttable collection of data
+print(list)
+tuple=(1,2,3,(4,5,6),("apple","mango")) #ordered and immutable collection of data
+print(tuple)
+dict1={"name":"mannu","age":20,"city":"delhi"} #unordered and mutable collection of data
+print(dict1)
+
