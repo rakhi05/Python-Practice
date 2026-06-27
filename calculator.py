7a11fabc0d7e5ed7a40913a5e02f58bd678e8e09
rakhi05 <rakhi662006@gmail.com>
2026-06-27 13:20:22 +0530

    python calculator

---
+++ calculator.py
@@ -0,0 +1,17 @@
+a=13
+b=5
+
+Ans1= a+b
+print("addition of a and b is",Ans1)
+Ans2= a-b
+print("subtraction of a and b is",Ans2)
+Ans3= a*b
+print("multiplication of a and b is",Ans3)
+Ans4= a/b
+print("division of a and b is",Ans4)
+Ans5= a//b
+print("floor division of a and b is",Ans5)
+Ans6= a%b
+print("modulus of a and b is",Ans6)
+Ans7= a**b
+print("exponentiation of a and b is",Ans7)
