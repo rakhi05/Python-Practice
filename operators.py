3a9da8b48f92e745b97b41ed90df7206e13d7925
rakhi05 <rakhi662006@gmail.com>
2026-06-27 13:16:22 +0530

    operators

---
+++ operators.py
@@ -0,0 +1,10 @@
+a=10
+b=2
+
+print(a+b)    #addition 
+print(a-b)    #substraction
+print(a*b)    #multiplication
+print(a/b)    #division
+print(a//b)   #floor division
+print(a%b)    #modulus
+print(a**b)   #exponentiation
