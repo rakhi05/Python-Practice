553896a33bacfcd3656d2c202f3282288201ba1e
rakhi05 <rakhi662006@gmail.com>
2026-06-28 12:34:31 +0530

    userinput

---
+++ userinput.py
@@ -0,0 +1,16 @@
+a=input()
+print(a)
+
+a=input("enter your name: ")
+print("my name is",a)
+
+
+
+x=input("enter first number: ")
+y=input("enter second number: ")
+print(x+y)
+
+
+x=input("enter first number: ")
+y=input("enter second number: ")
+print("the sum is",int(x)+int(y))
