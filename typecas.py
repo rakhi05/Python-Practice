23eccf85df04dff1903979b1ef0eeae46c13f987
rakhi05 <rakhi662006@gmail.com>
2026-06-28 12:33:31 +0530

    typecasting

---
+++ typecas.py
@@ -0,0 +1,21 @@
+#conversion of one datatype into another datatype is called typecasting
+
+a=1
+b=2
+print(a+b)    #addition
+
+a='10'
+b='20'
+print(a+b)    #concatenation
+
+#explicit typecasting
+a='10' #string
+b='20' #string
+print(int(a)+int(b))   #string to int conversion by me
+
+#implicit typecasting
+a=10 #int
+b=20.5 #float
+print(a+b)   #int to float conversion by python automatically
+
+
