5dc5c956da518fe5d72185fdafde8a65e892b02e
rakhi05 <rakhi662006@gmail.com>
2026-07-01 20:00:52 +0530

    string

---
+++ string.py
@@ -0,0 +1,33 @@
+name = 'mannu'
+friend = 'anu'
+line = ''' he said,
+hey mannu
+hey i am fine 
+i want to meet you'''
+
+print("hello," + name)
+
+# print name by index
+print(name[0]) 
+print(name[1])
+print(name[2])
+print(name[3])
+print(name[4])
+
+ #print(name[5]) throw an error
+
+print("lets use a for loop\n")
+for charater in line:
+    print(charater)
+
+fruit = 'banana'
+bananalen = len(fruit)
+print(bananalen)
+
+pie = "Applepie"
+print(pie[:5])
+print(pie[5:])
+print(pie[2:6])
+print(pie[-3:])
+
+
